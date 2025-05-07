23aaf7be2fdec2e28f041f63b7e89a3813251753
Faty-cmd <hello@nexors.ai>
2025-05-07 16:29:25 +0200

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Willkommen bei NEXORS</h1>
-      <a href="/demo">Jetzt Demo starten</a>
-    </main>
-  );
-}
