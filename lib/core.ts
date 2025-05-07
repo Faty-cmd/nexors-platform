e8c419bcb22450969f8421d1bd46e6da4a9d5520
Faty-cmd <hello@nexors.ai>
2025-05-07 17:58:15 +0200

    Delete lib directory

--- lib/core.ts
+++
@@ -1,7 +0,0 @@
-export function trackEvent(event: string) {
-  fetch("/api/track", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ event, timestamp: new Date().toISOString() })
-  });
-}
