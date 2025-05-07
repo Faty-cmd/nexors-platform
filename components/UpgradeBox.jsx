cd90076556d2f4595039afb2a528d1bb6ebb1d09
Faty-cmd <hello@nexors.ai>
2025-05-08 00:17:20 +0200

    Delete components directory

--- components/UpgradeBox.jsx
+++
@@ -1,9 +0,0 @@
-export default function UpgradeBox() {
-  return (
-    <div style={{ marginTop: 40, padding: 20, border: "1px solid #ccc" }}>
-      <h3>Upgrade auf NEXORS Pro?</h3>
-      <p>Schalte PDF-Export, CRM und mehr frei.</p>
-      <button style={{ marginTop: 10 }}>Jetzt upgraden</button>
-    </div>
-  );
-}
