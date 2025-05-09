d61e81a7a27ad0fa89b2ddd172d713b3d449e019
Faty-cmd <hello@nexors.ai>
2025-05-09 03:31:55 +0200

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header style={{ marginBottom: "2rem" }}>
+      <h1 style={{ fontSize: "2rem" }}>Willkommen bei <span style={{ color: "#4B8DF8" }}>NEXORS</span></h1>
+      <p>Das erste Super-KI-Wesen, das für dich denkt, spricht, verkauft & hilft.</p>
+    </header>
+  );
+}
